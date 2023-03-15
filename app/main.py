c81c31b16e51fd8370cf29562d54a08995d18810
David Pinner <david@fulcrumpro.com>
2023-03-14 18:58:57 -0700

    multi-ping

--- app/main.py
+++ app/main.py
@@ -5,9 +5,18 @@ def main():
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     conn, addr = server_socket.accept() # wait for client
+    print(f"Connected to {addr}")
     with conn:
-        print(f"Connected to {addr}")
-        conn.sendall(b'+PONG\r\n')
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                continue
+            resp = data.decode('utf-8').split()
+            if len(resp) > 0:
+                print(f"received {resp}")
+            for s in resp:
+                if s.lower() == "ping":
+                    conn.sendall(b'+PONG\r\n')
 
 
 if __name__ == "__main__":
