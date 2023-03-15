962a23da5e2c6b3d8b371044ba838c3d0ada30f3
David Pinner <david@fulcrumpro.com>
2023-03-14 18:24:13 -0700

    PONG

--- app/main.py
+++ app/main.py
@@ -4,7 +4,10 @@ import socket
 def main():
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    conn, addr = server_socket.accept() # wait for client
+    with conn:
+        print(f"Connected to {addr}")
+        conn.sendall(b'+PONG\r\n')
 
 
 if __name__ == "__main__":
