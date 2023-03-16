f01d2f3cce6c80d0f14e24c80ebb37bdbcd2b844
d-pinner <71914404+d-pinner@users.noreply.github.com>
2023-03-16 09:57:07 -0700

    event loop

--- app/main.py
+++ app/main.py
@@ -1,22 +1,33 @@
 import socket
 
+BUFFER_SIZE = 1024
+
+def parse(data):
+    decoded = data.decode('utf-8').split()
+    if len(decoded) > 0:
+        print(f"received {decoded}")
+    for s in decoded:
+        if s.lower() == "ping":
+            yield b'+PONG\r\n'
+
+
+def handle_client(conn):
+    while True:
+        data = conn.recv(BUFFER_SIZE)
+        if not data:
+            break
+        resp = parse(data)
+        while resp:
+            conn.sendall(resp)
+
 
 def main():
-
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    conn, addr = server_socket.accept() # wait for client
-    print(f"Connected to {addr}")
-    with conn:
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                continue
-            resp = data.decode('utf-8').split()
-            if len(resp) > 0:
-                print(f"received {resp}")
-            for s in resp:
-                if s.lower() == "ping":
-                    conn.sendall(b'+PONG\r\n')
+    while True:
+        conn, addr = server_socket.accept() # wait for client
+        print(f"Connected to {addr}")
+        with conn:
+            handle_client(conn)
 
 
 if __name__ == "__main__":
