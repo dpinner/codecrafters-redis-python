c769edf0d16838bc32dae5022607727959db9bb0
d-pinner <71914404+d-pinner@users.noreply.github.com>
2023-03-19 22:56:36 -0700

    get and set

--- app/server.py
+++ app/server.py
@@ -1,6 +1,6 @@
 import socket
 from selectors import BaseSelector, DefaultSelector, EVENT_READ, SelectorKey
-from typing import Dict, Optional
+from typing import Any, Dict, Optional
 from .serializer import RESPSerializer
 
 DEFAULT_BUFFER_SIZE = 1024
@@ -11,6 +11,7 @@ class RedisServer:
     _selector: BaseSelector
     _serializer: RESPSerializer
     _response_map: Dict[str,callable]
+    _cache: dict
 
     address: str
     port: int
@@ -23,8 +24,14 @@ class RedisServer:
         self._buffer = DEFAULT_BUFFER_SIZE if buf_size is None else buf_size
         self._selector = DefaultSelector()
         self._serializer = RESPSerializer('utf-8')
+        self._cache = {}
 
-        self._response_map = {"PING": self._ping, "ECHO": self._echo}
+        self._response_map = {
+            "PING": self._ping, 
+            "ECHO": self._echo, 
+            "SET": self._set, 
+            "GET": self._get
+        }
 
         self._server = socket.create_server((self.address, self.port), reuse_port=True)
         self._server.setblocking(False)
@@ -48,12 +55,11 @@ class RedisServer:
     def _handle_conn(self, conn: socket.socket, data: bytes):
         try:
             input = self._serializer.deserialize(data)
-            print(input)
-            if isinstance(input,str) and input.upper() == "PING":
-                conn.sendall(self._serializer.serialize("PONG"))
+            if isinstance(input,str) and input.upper() in self._response_map:
+                conn.sendall(self._response_map[input.upper()]())
             elif isinstance(input,list) and len(input) > 0:
                 cmd,args = input[0],input[1:]
-                if not isinstance(cmd,str):
+                if not isinstance(cmd,str) or cmd.upper() not in self._response_map:
                     raise ValueError("Unrecognized command")
                 conn.sendall(self._response_map[cmd.upper()](args))
             else:
@@ -65,14 +71,27 @@ class RedisServer:
     def _ping(self, *args):
         return self._serializer.serialize("PONG")
 
-    def _echo(self, input):
+    def _echo(self, input: list):
+        val = input[0] if len(input) == 1 else input
+        return self._serializer.serialize(val or "", bulk_str = True)
+
+    def _set(self, input: list):
+        if len(input) < 2:
+            raise ValueError("Usage: SET key val [EX expiry]")
+        key,val = input[:2]
+        if not isinstance(val,str):
+            raise ValueError("Cache values must be strings")
+        self._cache[key] = val
+        return self._serializer.serialize("OK")
+
+    def _get(self, input: list):
         if len(input) == 0:
-            val = ""
-        elif len(input) == 1:
-            val = input[0]
-        else:
-            val = input
-        return self._serializer.serialize(val, bulk_str = True)
+            raise ValueError("Usage: GET key")
+        val = self._cache.get(input[0])
+        if not (val is None or isinstance(val,str)):
+            raise ValueError("Cache values must be strings")
+        return self._serializer.serialize(val,bulk_str=True)
+
 
     def serve(self):
         self._selector.register(self._server, EVENT_READ, self._accept)
